2845e3fa0b58a6c8c3bd3cd25e4d0a9dba5896d4
AlanWang610 <mathwang2002@gmail.com>
2022-12-15 11:30:39 -0500

    Bootcamp 3 Part 3

--- src/index.js
+++ src/index.js
@@ -2,10 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, compose } from 'redux';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAfBZWUumw-qhllzVhSapaCrKBonoQy4c",
+    authDomain: "bootcamp-flashcards.firebaseapp.com",
+    databaseURL: "https://bootcamp-flashcards-default-rtdb.firebaseio.com",
+    projectId: "bootcamp-flashcards",
+    storageBucket: "bootcamp-flashcards.appspot.com",
+    messagingSenderId: "255677325107",
+    appId: "1:255677325107:web:f14fc4e90d5be05995a846"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const rootReducer = combineReducers({
+    firebase: firebaseReducer
+});
+  
+const store = createStore(rootReducer);
+
+const rrfConfig = {
+    userProfile: 'users'
+};
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch
+};
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ReactReduxFirebaseProvider>
+    </Provider>, 
     document.getElementById('root')
 );
